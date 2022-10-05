313121f430fe9ff84bde2afbab8b5589071be504
Usuario <Usuario@CPD03-M20>
2022-10-05 14:20:36 -0300

    Adicionar arquivos de projeto.

---
+++ ApP_guia/ApP_guia/Views/VerCompobebte.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ApP_guia.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VerCompobebte : ContentPage
+    {
+        public VerCompobebte()
+        {
+            InitializeComponent();
+        }
+    }
+}
